602f30fbda3853e4bf3666524acd7b8849f7c33a
FZhu <hzhu225@gmail.com>
2019-01-30 13:02:18 +1100

    Add WordAdapter class

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,14 +15,9 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -33,22 +28,36 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        ArrayList<String> words = new ArrayList<>();
-        words.add("one");
-        words.add("two");
-        words.add("three");
-        words.add("four");
-        words.add("five");
-        words.add("six");
-        words.add("seven");
-        words.add("eight");
-        words.add("nine");
-        words.add("ten");
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, R.layout.list_item, words);
+
+        WordAdapter wordAdapter = new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(itemsAdapter);
+        listView.setAdapter(wordAdapter);
 
+
+        /*
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add(("aaa"));
+        strings.add(("bbb"));
+        strings.add(("ccc"));
+        strings.add(("ddd"));
+        strings.add(("eee"));
+
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, strings);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemAdapter);
+        */
     }
 }
 
---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,39 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word>
+{
+    public WordAdapter(Context context, ArrayList<Word> resource)
+    {
+        super(context,0, resource);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        View listItemView = convertView;
+        if(listItemView == null)
+        {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+        TextView defaultTV = (TextView)listItemView.findViewById(R.id.default_text_view);
+        defaultTV.setText(currentWord.getDefaultTranslation());
+        TextView miwokTV = (TextView)listItemView.findViewById(R.id.miwok_text_view);
+        miwokTV.setText(currentWord.getMiwokTranslation());
+
+        return listItemView;
+    }
+}
