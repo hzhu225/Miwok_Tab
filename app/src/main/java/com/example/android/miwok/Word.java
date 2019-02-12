b56544e84ffa42bbf5ad8001678f0a2564bb78e3
FZhu <hzhu225@gmail.com>
2019-02-12 11:38:20 +1100

    Add image to list_item. Change Word object structure.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,11 +4,19 @@ public class Word
 {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId;
 
     public Word(String defaultTrans, String miworkTrans)
     {
         mDefaultTranslation = defaultTrans;
         mMiwokTranslation = miworkTrans;
+    }
+
+    public Word(String defaultTrans, String miworkTrans, int imageResourceId)
+    {
+        mDefaultTranslation = defaultTrans;
+        mMiwokTranslation = miworkTrans;
+        mImageResourceId = imageResourceId;
     }
 
     public String getDefaultTranslation()
@@ -21,4 +29,8 @@ public class Word
         return mMiwokTranslation;
     }
 
+    public int getImageResourceId()
+    {
+        return mImageResourceId;
+    }
 }
