9b06fe0eec2b8a8d46e6a8d59681e7f136e83434
FZhu <hzhu225@gmail.com>
2019-02-14 11:49:18 +1100

    Add audio to words

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,19 +5,22 @@ public class Word
     private String mDefaultTranslation;
     private String mMiwokTranslation;
     private int mImageResourceId;
+    private int mAudioResourceId;
 
-    public Word(String defaultTrans, String miworkTrans)
+    public Word(String defaultTrans, String miworkTrans, int audioResourceId)
     {
         mDefaultTranslation = defaultTrans;
         mMiwokTranslation = miworkTrans;
         mImageResourceId = -1;
+        mAudioResourceId = audioResourceId;
     }
 
-    public Word(String defaultTrans, String miworkTrans, int imageResourceId)
+    public Word(String defaultTrans, String miworkTrans, int imageResourceId, int audioResourceId)
     {
         mDefaultTranslation = defaultTrans;
         mMiwokTranslation = miworkTrans;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
     public String getDefaultTranslation()
@@ -33,5 +36,10 @@ public class Word
     public int getImageResourceId()
     {
         return mImageResourceId;
+    }
+
+    public int getAudioResourceId()
+    {
+        return mAudioResourceId;
     }
 }
