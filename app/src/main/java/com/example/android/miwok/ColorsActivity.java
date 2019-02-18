f0775c688a29c1c4ce2bb5e469a0b29e4ca5158c
FZhu <hzhu225@gmail.com>
2019-02-18 14:31:54 +1100

    Add 4 Fragment in 4 Activities

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,122 +1,21 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import java.util.ArrayList;
+import android.support.v7.app.AppCompatActivity;
 
-public class ColorsActivity extends AppCompatActivity {
-
-    private MediaPlayer player;
-    private MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener()
-    {
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer)
-        {
-            releaseMediaPlayer();
-        }
-    };
-    private AudioManager audioManager;
-
-    AudioManager.OnAudioFocusChangeListener audioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener()
-    {
-        @Override
-        public void onAudioFocusChange(int focusChange)
-        {
-            if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK)
-            {
-                player.pause();
-                player.seekTo(0);       //reverse player to beginning
-            }
-            else if(focusChange == AudioManager.AUDIOFOCUS_GAIN)
-            {
-                player.start();
-            }
-            else if(focusChange == AudioManager.AUDIOFOCUS_LOSS)
-            {
-                releaseMediaPlayer();
-            }
-        }
-    };
+public class ColorsActivity extends AppCompatActivity
+{
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        final ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red,  R.raw.color_red));
-        words.add(new Word("green", "chokokki", R.drawable.color_green,  R.raw.color_green));
-        words.add(new Word("brown", "ṭakaakki", R.drawable.color_brown,  R.raw.color_brown));
-        words.add(new Word("gray", "ṭopoppi", R.drawable.color_gray,  R.raw.color_gray));
-        words.add(new Word("black", "kululli", R.drawable.color_black,  R.raw.color_black));
-        words.add(new Word("white", "kelelli", R.drawable.color_white,  R.raw.color_white));
-        words.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow,  R.raw.color_dusty_yellow));
-        words.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow,  R.raw.color_mustard_yellow));
-
-        WordAdapter wordAdapter = new WordAdapter(this, words,R.color.category_colors);
-        ListView listView = (ListView) findViewById(R.id.word_listView);
-        listView.setAdapter(wordAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
-            {
-                Word word = words.get(position);
-                releaseMediaPlayer();
-                //Request audio focus
-                int result = audioManager.requestAudioFocus(audioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED)     //if we get audio focus now
-                {
-                    player = MediaPlayer.create(ColorsActivity.this, word.getAudioResourceId());
-                    player.start();
-                    player.setOnCompletionListener(completionListener);
-                }
-            }
-        });
-    }
-
-
-    @Override
-    protected void onStop()
-    {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-
-
-    private void releaseMediaPlayer()
-    {
-        if(player!= null)
-        {
-            player.release();
-            player = null;
-        }
-        audioManager.abandonAudioFocus(audioFocusChangeListener);
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new ColorsFragment())
+                .commit();
     }
 }
+
+
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,125 +1,17 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.support.v7.app.AppCompatActivity;
 
 public class PhrasesActivity extends AppCompatActivity {
-
-    private MediaPlayer player;
-    private MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener()
-    {
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer)
-        {
-            releaseMediaPlayer();
-        }
-    };
-    private AudioManager audioManager;
-
-    AudioManager.OnAudioFocusChangeListener audioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener()
-    {
-        @Override
-        public void onAudioFocusChange(int focusChange)
-        {
-            if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK)
-            {
-                player.pause();
-                player.seekTo(0);       //reverse player to beginning
-            }
-            else if(focusChange == AudioManager.AUDIOFOCUS_GAIN)
-            {
-                player.start();
-            }
-            else if(focusChange == AudioManager.AUDIOFOCUS_LOSS)
-            {
-                releaseMediaPlayer();
-            }
-        }
-    };
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        final ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
-        words.add(new Word("What is your name?", "tinnә oyaase'nә", R.raw.phrase_what_is_your_name));
-        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
-        words.add(new Word("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
-        words.add(new Word("I’m feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
-        words.add(new Word("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
-        words.add(new Word("Yes, I’m coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
-        words.add(new Word("I’m coming.", "әәnәm", R.raw.phrase_im_coming));
-        words.add(new Word("Let’s go.", "yoowutis", R.raw.phrase_lets_go));
-        words.add(new Word("Come here.", "әnni'nem", R.raw.phrase_come_here));
-
-
-        WordAdapter wordAdapter = new WordAdapter(this, words, R.color.category_phrases);
-        ListView listView = (ListView) findViewById(R.id.word_listView);
-        listView.setAdapter(wordAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
-            {
-                Word word = words.get(position);
-                releaseMediaPlayer();
-                //Request audio focus
-                int result = audioManager.requestAudioFocus(audioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED)     //if we get audio focus now
-                {
-                    player = MediaPlayer.create(PhrasesActivity.this, word.getAudioResourceId());
-                    player.start();
-                    player.setOnCompletionListener(completionListener);
-                }
-            }
-        });
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new PhrasesFragment())
+                .commit();
     }
 
 
-    @Override
-    protected void onStop()
-    {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-
-    private void releaseMediaPlayer()
-    {
-        if(player!= null)
-        {
-            player.release();
-            player = null;
-        }
-        audioManager.abandonAudioFocus(audioFocusChangeListener);
-    }
 }
